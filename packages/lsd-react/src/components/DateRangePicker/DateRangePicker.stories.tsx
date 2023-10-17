e81efc24ce4c19e49227f87634a7dae5dd8fbc6b
jongomez <jon.olive.gomez@gmail.com>
2023-10-18 00:29:28 +0100

    fix: rename outlined-bottom to underline and remove unused import

--- packages/lsd-react/src/components/DateRangePicker/DateRangePicker.stories.tsx
+++ packages/lsd-react/src/components/DateRangePicker/DateRangePicker.stories.tsx
@@ -14,7 +14,7 @@ export default {
     variant: {
       type: {
         name: 'enum',
-        value: ['outlined', 'outlined-bottom'],
+        value: ['outlined', 'underlined'],
       },
     },
   },
