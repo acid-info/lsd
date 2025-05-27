3adb2bc5187a9a75eaa6a03f00f78da1a94a4bd3
Xavier Saliniere <bonjour@xaviers.sh>
2025-05-27 04:08:38 -0400

    refactor(breadcrumb): write component style in pure css

--- packages/lsd-react/src/components/BreadcrumbItem/BreadcrumbItem.classes.ts
+++
@@ -1,9 +0,0 @@
-export const breadcrumbItemClasses = {
-  root: `lsd-breadcrumb-item`,
-
-  outlined: `lsd-breadcrumb-item--outlined`,
-  itemLink: `lsd-breadcrumb-item__link`,
-
-  large: `lsd-breadcrumb-item--large`,
-  small: `lsd-breadcrumb-item--small`,
-}
