be6d14499b947e674a028f1b34a4b62636913e88
Xavier Saliniere <bonjour@xaviers.sh>
2025-07-02 19:53:10 -0400

    fix(build): export as es module, include css into js entrypoint

--- packages/lsd-react/vite.config.ts
+++ packages/lsd-react/vite.config.ts
@@ -2,6 +2,39 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import type { OutputAsset, OutputChunk, OutputBundle } from 'rollup'
+
+function injectCssPlugin() {
+  return {
+    name: 'inject-css',
+    generateBundle(_: unknown, bundle: OutputBundle) {
+      let cssSource = ''
+      for (const fileName in bundle) {
+        if (fileName.endsWith('.css')) {
+          const cssFile = bundle[fileName] as OutputAsset
+          cssSource += cssFile.source
+          delete bundle[fileName]
+        }
+      }
+
+      if (cssSource) {
+        this.emitFile({
+          type: 'asset',
+          fileName: 'index.css',
+          source: cssSource,
+        })
+
+        for (const fileName in bundle) {
+          const file = bundle[fileName]
+          if (fileName.endsWith('.js') && 'isEntry' in file && file.isEntry) {
+            const jsFile = file as OutputChunk
+            jsFile.code = `import './index.css';\n${jsFile.code}`
+          }
+        }
+      }
+    },
+  }
+}
 
 export default defineConfig({
   plugins: [
@@ -11,24 +44,20 @@ export default defineConfig({
     }),
   ],
 
+  css: { modules: { localsConvention: 'camelCaseOnly' } },
   build: {
-    minify: false,
+    minify: true,
     outDir: 'dist',
-
     lib: {
       entry: path.resolve(__dirname, './src/index.ts'),
       name: 'lsd-react',
       fileName: 'lsd-react',
-      formats: ['cjs', 'umd'],
+      formats: ['es'],
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
+      output: { globals: { react: 'React', 'react-dom': 'ReactDOM' } },
+      plugins: [injectCssPlugin()],
     },
   },
 })
